48cf8bf52431995294bbb311da828bf0735e3db2
CodeWithJalloh <codewithjalloh@gmail.com>
2017-07-17 20:57:42 +0100

    take picture from camera and photo library added

--- Swift Machine Learning/ViewController.swift
+++ Swift Machine Learning/ViewController.swift
@@ -20,13 +20,55 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
     }
     
     
+    func detectImageContent() {
+        
+    }
+    
+    
+    // take a picture from your camera
     @IBAction func takePhoto(_ sender: Any) {
+        
+        if UIImagePickerController.isSourceTypeAvailable(.camera) {
+            let imagePicker = UIImagePickerController()
+            imagePicker.delegate = self
+            imagePicker.sourceType = .camera
+            imagePicker.allowsEditing = false
+            self.present(imagePicker, animated: true, completion: nil)
+        }
+        
     }
     
     
+    // select from photoLibrary
     @IBAction func photoLibrary(_ sender: Any) {
+        
+        if UIImagePickerController.isSourceTypeAvailable(.photoLibrary) {
+            let imgPick = UIImagePickerController()
+            imgPick.delegate = self
+            imgPick.sourceType = .photoLibrary
+            imgPick.allowsEditing = true
+            self.present(imgPick, animated: true, completion: nil)
+        }
+        
     }
     
+    
+    
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        
+        if let pickedImg = info[UIImagePickerControllerOriginalImage] as? UIImage {
+            
+            photoView.contentMode = .scaleToFill
+            photoView.image = pickedImg
+        }
+        
+        picker.dismiss(animated: true, completion: nil)
+        detectImageContent()
+        
+        
+        
+    }
     
     
 
